263c47603f2310065ef12a2c81b59031d966dc07
onura <onur.akdogan51@gmail.com>
2022-01-11 00:58:49 +0300

    added Number function

--- src/App.js
+++ src/App.js
@@ -11,26 +11,26 @@ import ToDoForm from './ToDoForm';
 
 const App = () => {
   
-  const [ toDoList, setToDoList ] = useState(data);
+  const [ toDoList, settoDoList ] = useState(data);
 
   const handleToggle = (id) => {
     let mapped = toDoList.map(title => {
-      return title.id === id ? { ...title, complete: !title.complete } : { ...title};
+      return title.id === Number(id) ? { ...title, complete: !title.complete } : { ...title};
     });
-    setToDoList(mapped);
+    settoDoList(mapped);
   }
 
   const handleFilter = () => {
     let filtered = toDoList.filter(title => {
       return !title.complete;
     });
-    setToDoList(filtered);
+    settoDoList(filtered);
   }
 
   const addTask = (InputVal ) => {
     let copy = [...toDoList];
     copy = [...copy, { id: toDoList.length + 1, title: InputVal, complete: false }];
-    setToDoList(copy);
+    settoDoList(copy);
   }
 
 
